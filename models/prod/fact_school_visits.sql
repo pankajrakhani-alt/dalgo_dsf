bcbfa06ae2f4aad27c9f3c9551b93e2fb009372c
pankajrakhani-alt <pankaj.rakhani@gmail.com>
2026-02-09 14:25:14 +0530

    Add DPDP-safe prod fact table for school visits

--- models/prod/fact_school_visits.sql
+++ models/prod/fact_school_visits.sql
@@ -1,1 +1,28 @@
+select
+    visit_date,
+    cohort,
+    school_name,
+
+    count(*) as total_visits,
+
+    sum(
+        coalesce(boys_session1, 0)
+        + coalesce(girls_session1, 0)
+    ) as total_students,
+
+    avg(interaction_rating) as avg_interaction_rating,
+
+    sum(
+        case
+            when teacher_present_session1 = 'Yes' then 1
+            else 0
+        end
+    ) as teacher_present_visits
+
+from {{ ref('stg_surveycto_visits') }}
+
+group by
+    visit_date,
+    cohort,
+    school_name
 
