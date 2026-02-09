cefc821a607936f7aed88d4d776a3f421c86ff79
pankajrakhani-alt <pankaj.rakhani@gmail.com>
2026-02-09 14:09:21 +0530

    Remove old staging model (cleanup)

--- models/staging/staging_pc.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='table') }}
-SELECT DISTINCT
-    TRIM(LOWER(name_of_fellow)) as name_of_fellow,
-    COUNT(*) AS visit_count
-FROM {{ source('staging', 'merged_p_c') }}
-GROUP BY TRIM(LOWER(name_of_fellow))
-ORDER BY visit_count DESC
