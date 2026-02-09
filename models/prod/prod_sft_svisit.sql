25048572d402bdd885b8bbf6078d4fd55717e8c9
pankajrakhani-alt <pankaj.rakhani@gmail.com>
2026-02-09 13:55:45 +0530

    Remove old prod model (cleanup)

--- models/prod/prod_sft_svisit.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-SELECT
-    COALESCE(a.name, p.name_of_fellow) AS name,
-    a.visit_count AS actual_visits,
-    p.visit_count AS planned_visits
-FROM {{ ref('staging_as') }} a
-FULL OUTER JOIN {{ ref('staging_ps') }} p
-    ON TRIM(LOWER(a.name)) = TRIM(LOWER(p.name_of_fellow))
-ORDER BY name
