36287ad95f3480617171df949ac52f65d8838e23
pankajrakhani-alt <pankaj.rakhani@gmail.com>
2026-02-13 12:41:27 +0530

    Update stg_surveycto_visits.sql

--- models/staging/stg_surveycto_visits.sql
+++ models/staging/stg_surveycto_visits.sql
@@ -16,7 +16,7 @@ with source as (
         cast(boys_session_1 as integer) as boys_session1,
         cast(girls_session_1 as integer) as girls_session1,
         cast(interaction_rating as integer) as interaction_rating,
-        teacher_present_session1,
+        teacher_present_session_1,
         interact_with_teacher
 
     from {{ source('staging', 'pp_raw_data') }}
