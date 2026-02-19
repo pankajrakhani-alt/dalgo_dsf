cb911f4aa80d1b079924244f786dd54033bc9fb7
Pankaj Rakhani <Pankaj.rakhani@danisports.org>
2026-02-19 12:47:17 +0530

    Final staging cleaning and deduplication logic

--- models/staging/stg_surveycto_visits.sql
+++ models/staging/stg_surveycto_visits.sql
@@ -12,10 +12,39 @@ with source as (
             school_name_zp
         ) as school_name,
 
-        cast(sessions_taken_today as integer) as sessions_taken_today,
-        cast(boys_session_1 as integer) as boys_session1,
-        cast(girls_session_1 as integer) as girls_session1,
-        cast(interaction_rating as integer) as interaction_rating,
+        -- Clean sessions_taken_today
+        case 
+            when sessions_taken_today is null then 0
+            when trim(sessions_taken_today) ~ '^[0-9]+$' 
+                then cast(trim(sessions_taken_today) as integer)
+            when lower(trim(sessions_taken_today)) = 'yes' 
+                then 1
+            when lower(trim(sessions_taken_today)) = 'no' 
+                then 0
+            else 0
+        end as sessions_taken_today,
+
+        -- Clean boys_session_1
+        case 
+            when trim(boys_session_1) ~ '^[0-9]+$'
+                then cast(trim(boys_session_1) as integer)
+            else 0
+        end as boys_session1,
+
+        -- Clean girls_session_1
+        case 
+            when trim(girls_session_1) ~ '^[0-9]+$'
+                then cast(trim(girls_session_1) as integer)
+            else 0
+        end as girls_session1,
+
+        -- Clean interaction_rating
+        case 
+            when trim(interaction_rating) ~ '^[0-9]+$'
+                then cast(trim(interaction_rating) as integer)
+            else null
+        end as interaction_rating,
+
         teacher_present_session_1,
         interact_with_teacher,
         activities_session_1 as activities_session1
@@ -25,6 +54,7 @@ with source as (
 ),
 
 deduplicated as (
+
     select *
     from (
         select *,
@@ -35,6 +65,7 @@ deduplicated as (
         from source
     ) t
     where rn = 1
+
 )
-    
+
 select * from deduplicated
