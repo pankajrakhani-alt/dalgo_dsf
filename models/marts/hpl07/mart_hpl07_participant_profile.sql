cbcfb62195a5b0d4c186bea4458212fb4ea55195
Pankaj Rakhani <Pankaj.rakhani@danisports.org>
2026-05-26 12:28:14 +0530

    feat: hpl07 state_name field added in mart

--- models/marts/hpl07/mart_hpl07_participant_profile.sql
+++ models/marts/hpl07/mart_hpl07_participant_profile.sql
@@ -12,7 +12,45 @@ final as (
         gender,
         dob,
         age,
-        state,
+        case state
+            when 'an' then 'Andaman & Nicobar Islands'
+            when 'ap' then 'Andhra Pradesh'
+            when 'ar' then 'Arunachal Pradesh'
+            when 'as' then 'Assam'
+            when 'br' then 'Bihar'
+            when 'ch' then 'Chandigarh'
+            when 'ct' then 'Chhattisgarh'
+            when 'dd' then 'Dadra & Nagar Haveli and Daman & Diu'
+            when 'dl' then 'Delhi'
+            when 'ga' then 'Goa'
+            when 'gj' then 'Gujarat'
+            when 'hp' then 'Himachal Pradesh'
+            when 'hr' then 'Haryana'
+            when 'jh' then 'Jharkhand'
+            when 'jk' then 'Jammu & Kashmir'
+            when 'ka' then 'Karnataka'
+            when 'kl' then 'Kerala'
+            when 'la' then 'Ladakh'
+            when 'ld' then 'Lakshadweep'
+            when 'mh' then 'Maharashtra'
+            when 'ml' then 'Meghalaya'
+            when 'mn' then 'Manipur'
+            when 'mp' then 'Madhya Pradesh'
+            when 'mz' then 'Mizoram'
+            when 'nl' then 'Nagaland'
+            when 'or' then 'Odisha'
+            when 'pb' then 'Punjab'
+            when 'py' then 'Puducherry'
+            when 'rj' then 'Rajasthan'
+            when 'sk' then 'Sikkim'
+            when 'tn' then 'Tamil Nadu'
+            when 'tg' then 'Telangana'
+            when 'tr' then 'Tripura'
+            when 'uk' then 'Uttarakhand'
+            when 'up' then 'Uttar Pradesh'
+            when 'wb' then 'West Bengal'
+            else state
+        end                                         as state_name,
         city,
 
         -- 🏢 Professional
